d16ef88203ed8ef0d222e9fba4de51b129f93066
Ian Dees <ian.dees@gmail.com>
2013-11-09 20:10:09 -0600

    Trying to hack around the way Go does XML and JSON marshalling.

--- api.go
+++ api.go
@@ -1,12 +1,101 @@
-package goosm
+package main
 
 import (
+    "encoding/xml"
+    "encoding/json"
     "github.com/gorilla/mux"
     "net/http"
 )
 
-func CapabilitiesHandler(w http.ResponseWriter, r *http.Request) {
+type Tag struct {
+    XMLName     xml.Name    `xml:"tag" json:"-"`
+    Key         string      `xml:"k,attr"`
+    Value       string      `xml:"v,attr"`
+}
 
+type Changeset struct {
+    XMLName     xml.Name    `xml:"changeset" json:"-"`
+    Id          int32       `xml:"id,attr"`
+    CreatedAt   string      `xml:"created_at,attr"`
+    ClosedAt    string      `xml:"closed_at,attr"`
+    User        string      `xml:"user,attr"`
+    Uid         int32       `xml:"uid,attr"`
+    Tags        []Tag       `xml:"tag"`
+    MinLat      float32     `xml:"min_lat,attr"`
+    MinLon      float32     `xml:"min_lon,attr"`
+    MaxLat      float32     `xml:"max_lat,attr"`
+    MaxLon      float32     `xml:"max_lon,attr"`
+}
+
+type Node struct {
+    XMLName     xml.Name    `xml:"node" json:"-"`
+    Id          int64       `xml:"id,attr" json:"id"`
+    Version     int16       `xml:"version,attr" json:"version,attr"`
+    Changeset   int32       `xml:"changeset,attr" json:"changeset,attr"`
+    User        string      `xml:"user,attr" json:"user,attr"`
+    Uid         int32       `xml:"uid,attr" json:"uid,attr"`
+    Visible     bool        `xml:"visible,attr" json:"visible,attr"`
+    Timestamp   string      `xml:"timestamp,attr" json:"timestamp,attr"`
+    Tags        []Tag       `xml:"tag" json:"tag"`
+    Lat         float32     `xml:"lat,attr" json:"lat,attr"`
+    Lon         float32     `xml:"lon,attr" json:"lon,attr"`
+}
+
+type Way struct {
+    XMLName     xml.Name    `xml:"way" json:"-"`
+    Id          int64       `xml:"id,attr"`
+    Version     int16       `xml:"version,attr"`
+    Changeset   int32       `xml:"changeset,attr"`
+    User        string      `xml:"user,attr"`
+    Uid         int32       `xml:"uid,attr"`
+    Visible     bool        `xml:"visible,attr"`
+    Timestamp   string      `xml:"timestamp,attr"`
+    Tags        []Tag       `xml:"tag"`
+    Nds         []Nd        `xml:"nd"`
+}
+
+type Nd struct {
+    XMLName     xml.Name    `xml:"nd" json:"-"`
+    Ref         string      `xml:"ref,attr"`
+}
+
+type Relation struct {
+    XMLName     xml.Name    `xml:"relation" json:"-"`
+    Id          int64       `xml:"id,attr"`
+    Version     int16       `xml:"version,attr"`
+    Changeset   int32       `xml:"changeset,attr"`
+    User        string      `xml:"user,attr"`
+    Uid         int32       `xml:"uid,attr"`
+    Visible     bool        `xml:"visible,attr"`
+    Timestamp   string      `xml:"timestamp,attr"`
+    Tags        []Tag       `xml:"tag"`
+    Members     []Member    `xml:"member"`
+}
+
+type Member struct {
+    XMLName     xml.Name    `xml:"member" json:"-"`
+    Type        string      `xml:"type,attr"`
+    Ref         string      `xml:"ref,attr"`
+    Role        string      `xml:"role,attr"`
+}
+
+type Capabilities struct {
+    MinimumVersion string `xml:"api>version>minimum,attr"`
+    MaxmimumVersion string `xml:"api>version>maximum,attr"`
+}
+
+func CapabilitiesHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/xml")
+    w.Write([]byte(`<?xml version="1.0" encoding="UTF-8"?>
+<osm version="0.6" generator="goosm">
+    <api>
+        <version minimum="0.6" maximum="0.6"/>
+        <area maximum="0.25"/>
+        <waynodes maximum="2000"/>
+        <changesets maximum_elements="50000"/>
+        <timeout seconds="300"/>
+    </api>
+</osm>`))
 }
 
 func MapHandler(w http.ResponseWriter, r *http.Request) {
@@ -54,7 +143,20 @@ func CreateNodeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetNodeHandler(w http.ResponseWriter, r *http.Request) {
+    node := Node{Id:1214, Version:1, Changeset:124, User:"iandees", Uid:4732, Visible:true, Timestamp:"foo", Tags:[]Tag{}, Lat:45.1, Lon:-90.2}
+    
+    if (true) {
+        w.Write([]byte(`<?xml version="1.0" encoding="UTF-8"?>`))
+        w.Write([]byte(`<osm version="0.6" generator="goosm">`))
 
+        enc := xml.NewEncoder(w)
+        enc.Encode(node)
+
+        w.Write([]byte("</osm>\n"))
+    } else {
+        enc := json.NewEncoder(w)
+        enc.Encode(node)
+    }
 }
 
 func UpdateNodeHandler(w http.ResponseWriter, r *http.Request) {
@@ -90,7 +192,15 @@ func CreateWayHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetWayHandler(w http.ResponseWriter, r *http.Request) {
+    way := Way{Id:1214, Version:1, Changeset:124, User:"iandees", Uid:4732, Visible:true, Timestamp:"foo"}
+    
+    w.Write([]byte(`<?xml version="1.0" encoding="UTF-8"?>
+<osm version="0.6" generator="goosm">`))
 
+    enc := json.NewEncoder(w)
+    enc.Encode(way)
+
+    w.Write([]byte("</osm>\n"))
 }
 
 func UpdateWayHandler(w http.ResponseWriter, r *http.Request) {
@@ -156,7 +266,6 @@ func RelationFullHandler(w http.ResponseWriter, r *http.Request) {
 func RelationMultifetchHandler(w http.ResponseWriter, r *http.Request) {
 
 }
-
 
 func main() {
     r := mux.NewRouter()
